1c1c1d28caf07a9f07e7f52367e555f740a1bebb
Jeffrey Hu <zhiwehu@gmail.com>
2017-01-31 18:07:39 +0800

    update style

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 引入React和Component
 import React, { Component } from "react";
 // 引入View，类似于html的Div
-import { View, Text, } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 // 引入我们的Header模块
 import Header from "./header";
 // 引入我们的Footer模块
@@ -15,16 +15,33 @@ class App extends Component {
    */
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Header />
-        <View>
+
+        <View style={styles.content}>
           <Text>我是Content</Text>
         </View>
+
         <Footer />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    ...Platform.select({
+      ios: {
+        paddingTop: 30
+      }
+    })
+  },
+  content: {
+    flex: 1
+  }
+});
 
 // 导出这个模块，供外部调用
 export default App;
