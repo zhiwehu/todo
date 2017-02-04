6e1f2134972dbe64df0dcbdcd1fcf19e8ac7cf6b
Jeffrey Hu <zhiwehu@gmail.com>
2017-02-05 07:20:10 +0800

    add TextInput

--- app.js
+++ app.js
@@ -9,6 +9,40 @@ import Footer from "./footer";
 
 // 定义App类，这个类是Component的子类
 class App extends Component {
+
+  // 构造方法,初始化state
+  constructor(props) {
+    super(props);
+    // 初始化2个状态
+    this.state = {
+      value: "",
+      items: []
+    };
+  }
+
+  /*
+   传给Header.TextInput.onSubmitEditing的回调函数
+   更新this.state.items
+   设置this.state.value为空
+   */
+  handleAddItem() {
+    if (!this.state.value) return;
+    // 创建一个新的items,从this.state.items里copy现有的数据,再增加一个新的
+    const newItems = [
+      ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete: false
+      }
+    ];
+    // 更新state
+    this.setState({
+      items: newItems,
+      value: ""
+    });
+  }
+
   /*
    实现App类的render方法，这个方法返回一个View，
    其组成分别是Header, Content和Footer
@@ -16,7 +50,11 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Header />
+        <Header
+          value = {this.state.value}
+          onAddItem = {this.handleAddItem.bind(this)}
+          onChange = {(value) => this.setState({value})}
+        />
 
         <View style={styles.content}>
           <Text>我是Content</Text>
--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 // 引入React和Component
-import React, { Component } from "react";
+import React, {Component} from "react";
 // 引入Text，显示文字
-import { View, Text } from "react-native";
+import {View, Text, StyleSheet, TextInput} from "react-native";
 
 // 定义Header类，这个类是Component的子类
 class Header extends Component {
@@ -10,12 +10,34 @@ class Header extends Component {
    */
   render() {
     return (
-      <View>
-        <Text>我是Header</Text>
+      <View style={styles.header}>
+        <TextInput
+          placeholder="什么需要做?"
+          value={this.props.value}
+          onChangeText={this.props.onChange}
+          onSubmitEditing={this.props.onAddItem}
+          blurOnSubmit={false}
+          returnKeyType="done"
+          style={styles.input}
+        />
       </View>
     );
   }
 }
+
+// 创建StyleSheet
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  input: {
+    flex: 1,
+    height: 50
+  }
+});
 
 // 导出这个模块，供外部调用
 export default Header;
